7715eab7997d4eee082774022e96ed197f06e7ea
Md. Ariful Islam <37192681+Aquib-Ibn-Amin@users.noreply.github.com>
2024-06-02 05:16:14 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,32 @@
+const accountId = 144553
+let accountEmail = "webaquib@gmail.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+let accountState;
+
+// accountId = 2 // not allowed
+
+if (condition) {
+    
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
+
+accountEmail = "aq@gmail.com"
+accountPassword = "16127"
+accountCity = "Basila"
+
+console.log(accountId);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
