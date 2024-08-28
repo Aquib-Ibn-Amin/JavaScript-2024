b1dad82345e2febbc33d020c5502c3f5b7325623
Md. Ariful Islam <37192681+Aquib-Ibn-Amin@users.noreply.github.com>
2024-08-28 12:32:12 +0000

    string to number

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -83,3 +83,32 @@ num1 = num2 = num3 = 2 + 2
 let gameCounter = 100
 ++gameCounter;
 console.log(gameCounter);
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " aquib"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+ "2");
+console.log("1" + 2 +2);
+console.log(1 + 2  + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(ture);
+// console.log(+ture);
+// console.log(ture+);
+// console.log(+"");
